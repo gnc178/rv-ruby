10927b3ea0e23149a39f73b3082ab415a5d42b7c
gnc178 <175585293+gnc178@users.noreply.github.com>
2026-04-01 12:26:53 +0500

    Create rv-ruby@2.1.10.rb

---
+++ Formula/rv-ruby@2.1.10.rb
@@ -0,0 +1,6 @@
+require File.expand_path("../Abstract/rv-ruby-21", __dir__)
+
+class RvRubyAT2110 < RvRuby21
+  url "https://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.10.tar.xz"
+  sha256 "5be9f8d5d29d252cd7f969ab7550e31bbb001feb4a83532301c0dd3b5006e148"
+end
